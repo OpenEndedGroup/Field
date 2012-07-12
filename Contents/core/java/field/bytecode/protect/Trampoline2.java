4a4c03df11dc6a8c189f9a40491e4da4fb86fc7f
marc <marc@openendedgroup.com>
2012-07-12 18:21:34 -0500

    look for .so files as well as .dylib

--- Contents/core/java/field/bytecode/protect/Trampoline2.java
+++ Contents/core/java/field/bytecode/protect/Trampoline2.java
@@ -159,8 +159,8 @@ public class Trampoline2 implements iLaunchable {
 
 		@Override
 		protected String findLibrary(String rawName) {
-			;//System.out.println(" -- find library :" + rawName + " -- ");
 
+			{
 			String name = "lib" + rawName + ".dylib";
 
 			for (String s : extendedLibraryPaths) {
@@ -177,7 +177,26 @@ public class Trampoline2 implements iLaunchable {
 					return file.getAbsolutePath();
 				}
 			}
-			return super.findLibrary(name);
+			}
+			{
+			String name = "lib" + rawName + ".so";
+
+			for (String s : extendedLibraryPaths) {
+				File file = new File(s, name);
+				if (file.exists()) {
+					;//System.out.println(" found it <" + file + ">");
+					return file.getAbsolutePath();
+				}
+			}
+			for (String s : extendedClassPaths) {
+				File file = new File(s, name);
+				if (file.exists()) {
+					;//System.out.println(" found it <" + file + ">");
+					return file.getAbsolutePath();
+				}
+			}
+			}
+			return super.findLibrary(rawName);
 		}
 
 		LinkedHashSet<String> knownPackages = new LinkedHashSet<String>();
@@ -804,7 +823,7 @@ public class Trampoline2 implements iLaunchable {
 		trampoline = this;
 
 		String exceptions = SystemProperties.getProperty("trampolineExceptions", null);
-		ignored = new String[] { "apple.","java.", "javax.", "sun.", "com.apple", "app	le.", "field.namespace", "field.math", "field.launch.", "org.objectweb", "com.sun", "org.xml", "org.w3c", "$Prox", "org.eclipse", "main", "field.util.BetterWeak", "field.misc.ANSIColorUtils", "ch.rand", "org.python", "org.apache.batik", "org.antlr", "field.util.TaskQueue", "com.lowagie", "net.sf.cglib.proxy", "com.seaglasslookandfeel", "org.pushingpixels", "net.sourceforge.napkinlaf.", "prosilica"/*
+		ignored = new String[] { "apple.","java.", "javax.", "sun.", "com.apple", "app	le.", "field.namespace", "field.math", "field.launch.", "org.objectweb", "com.sun", "org.xml", "org.w3c", "$Prox", "org.eclipse", "main", "field.util.BetterWeak", "field.misc.ANSIColorUtils", "ch.rand", "org.python", "org.apache.batik", "org.antlr", "field.util.TaskQueue", "com.lowagie", "net.sf.cglib.proxy", "com.seaglasslookandfeel", "org.pushingpixels", "net.sourceforge.napkinlaf." /*
 																																																												 * ,
 																																																												 * "com.sun.jna"
 																																																												 */, "com.kenai.jaffl"};
